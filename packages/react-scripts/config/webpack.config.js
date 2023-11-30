53b16293b8d81470a27266dff4a716bfa90afde9
AlSharp <elistaphilly@gmail.com>
2023-11-29 19:14:09 -0500

    build multiple index html files [skip-ci]

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -95,6 +95,14 @@ const hasJsxRuntime = (() => {
   }
 })();
 
+// siteConfig
+const siteConfig = require(path.join(
+  paths.appPath,
+  'src',
+  'config',
+  'site.config.json'
+));
+
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
 module.exports = function (webpackEnv) {
@@ -604,31 +612,36 @@ module.exports = function (webpackEnv) {
       ].filter(Boolean),
     },
     plugins: [
-      // Generates an `index.html` file with the <script> injected.
-      new HtmlWebpackPlugin(
-        Object.assign(
-          {},
-          {
-            inject: true,
-            template: paths.appHtml,
-          },
-          isEnvProduction
-            ? {
-                minify: {
-                  removeComments: true,
-                  collapseWhitespace: true,
-                  removeRedundantAttributes: true,
-                  useShortDoctype: true,
-                  removeEmptyAttributes: true,
-                  removeStyleLinkTypeAttributes: true,
-                  keepClosingSlash: true,
-                  minifyJS: true,
-                  minifyCSS: true,
-                  minifyURLs: true,
-                },
-              }
-            : undefined
-        )
+      // Generates an `index.html` files with the <script> injected.
+      ...siteConfig.webpack.languages.map(
+        langObj =>
+          new HtmlWebpackPlugin(
+            Object.assign(
+              {},
+              {
+                filename: langObj.filename,
+                inject: true,
+                template: paths.appHtml,
+                meta: langObj.meta,
+              },
+              isEnvProduction
+                ? {
+                    minify: {
+                      removeComments: true,
+                      collapseWhitespace: true,
+                      removeRedundantAttributes: true,
+                      useShortDoctype: true,
+                      removeEmptyAttributes: true,
+                      removeStyleLinkTypeAttributes: true,
+                      keepClosingSlash: true,
+                      minifyJS: true,
+                      minifyCSS: true,
+                      minifyURLs: true,
+                    },
+                  }
+                : undefined
+            )
+          )
       ),
       // Inlines the webpack runtime script. This script is too small to warrant
       // a network request.
